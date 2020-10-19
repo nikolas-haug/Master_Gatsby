0eb3460d768e4b399db0d8ce2aa66beeb7a5589b
Nikolas Haug <nikohaug21@gmail.com>
2020-10-19 16:30:01 -0500

    add sanity config

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,36 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+    const pizzas = data.pizzas.nodes;
     return (
          <>
-            <p>Hey! I'm the Pizzas page</p>
+            <p>Hey! There are { pizzas.length } Pizzas!!! </p>
         </>
     )
 }
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image {
+                    asset {
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
