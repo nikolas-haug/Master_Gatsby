eb980be430e1a8b1484ee5f80a6ecb0d9341d856
Nikolas Haug <nikohaug21@gmail.com>
2020-10-28 15:19:58 -0500

    add calculate order utility

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -9,6 +9,7 @@ import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
 import usePizza from '../utils/usePizza';
 import PizzaOrder from '../components/PizzaOrder';
+import calculateOrderTotal from '../utils/calculateOrderTotal';
 
 export default function OrderPage({ data }) {
     const pizzas = data.pizzas.nodes;
@@ -67,6 +68,10 @@ export default function OrderPage({ data }) {
                 <fieldset className="order">
                     <legend>Order</legend>
                     <PizzaOrder order={order} removeFromOrder={removeFromOrder} pizzas={pizzas} />
+                </fieldset>
+                <fieldset>
+                    <h3>Your Total is {formatMoney(calculateOrderTotal(order, pizzas))}</h3>
+                    <button type="submit">Order Ahead</button>
                 </fieldset>
             </OrderStyles>
         </>
