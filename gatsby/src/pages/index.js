d3bf1d2ab5e9fc12da443cee69a696455bdd30ec
Nikolas Haug <nikohaug21@gmail.com>
2020-11-11 11:25:08 -0600

    add loading grid for homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid } from '../styles/Grids';
 import useLatestData from '../utils/useLatestData';
 
-function CurrentlySlicing() {
+function CurrentlySlicing({ slicemasters }) {
+    console.log(slicemasters);
     return (
         <div>
-            <p>Currently Slicing</p>
+            {!slicemasters && <LoadingGrid count={4} />}
+            {slicemasters && !slicemasters?.length && <p>No one is working right now</p>}
         </div>
     )
 }
 
-function HotSlices() {
+function HotSlices({ hotSlices }) {
     return (
         <div>
-            <p>Hot Slices</p>
+            {!hotSlices && <LoadingGrid count={4} />}
+            {hotSlices && !hotSlices?.length && <p>Nothin' in the case</p>}
         </div>
     )
 }
@@ -24,8 +29,10 @@ export default function HomePage() {
             <h1>The Best Pizza Downtown</h1>
             <p>Open 11am to 11pm Every Single Day</p>
             <div>
-                <CurrentlySlicing slicemasters={slicemasters} />
-                <HotSlices hotSlices={hotSlices} />
+                <HomePageGrid>
+                    <CurrentlySlicing slicemasters={slicemasters} />
+                    <HotSlices hotSlices={hotSlices} />
+                </HomePageGrid>
             </div>
         </div>
     ) 
