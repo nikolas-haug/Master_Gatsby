512deb7abab6e30105ae995255ba39b4f3540d44
Nikolas Haug <nikohaug21@gmail.com>
2020-11-11 10:48:07 -0600

    add client side data fetch utility

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+    return (
+        <div>
+            <p>Currently Slicing</p>
+        </div>
+    )
+}
+
+function HotSlices() {
+    return (
+        <div>
+            <p>Hot Slices</p>
+        </div>
+    )
+}
 
 export default function HomePage() {
+    const { slicemasters, hotSlices } = useLatestData();
     return (
-        <>
-            <p>Hey! I'm the home page</p>
-            <p>Hey! I'm another element</p>
-        </>
+        <div className="center">
+            <h1>The Best Pizza Downtown</h1>
+            <p>Open 11am to 11pm Every Single Day</p>
+            <div>
+                <CurrentlySlicing slicemasters={slicemasters} />
+                <HotSlices hotSlices={hotSlices} />
+            </div>
+        </div>
     ) 
 }
