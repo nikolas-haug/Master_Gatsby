62d00fb8aa31547d4e812e57ca157865d3340ca9
Nikolas Haug <nikohaug21@gmail.com>
2020-11-12 16:50:20 -0600

    add vercel deploy comments to functions

--- gatsby/functions/hello/hello.js
+++ gatsby/functions/hello/hello.js
@@ -1,3 +1,9 @@
+// Vercel modifications
+// module.exports = async (req, res) => ({
+//     statusCode: 200,
+//     body: 'Hello!'
+// });
+
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
