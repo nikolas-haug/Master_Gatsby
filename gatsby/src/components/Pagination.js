85b19f8ac9201b2e99c1fa66012499aa7ddfe4e1
Nikolas Haug <nikohaug21@gmail.com>
2020-11-12 10:30:07 -0600

    add media queries

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -26,6 +26,12 @@ const PaginationStyles = styled.div`
             color: var(--grey);
         }
     }
+    @media (max-width: 800px) {
+        .word {
+            display: none;
+        }
+        font-size: 1.4rem;
+    }
 `;
 
 export default function Pagination({ pageSize, totalCount, currentPage, skip, base }) {
@@ -37,12 +43,12 @@ export default function Pagination({ pageSize, totalCount, currentPage, skip, ba
     const hasPrevPage = prevPage >= 1;
     return (
         <PaginationStyles>
-            <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
-                &larr; Prev
+            <Link title="Previous page" disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+                &larr; <span className="word">Prev</span> 
             </Link>
-                {Array.from({ length: totalPages }).map((_, i) => <Link key={i} className={currentPage === 1 && i === 0 ? 'current' : ''} to={`${base}/${i > 0 ? i + 1 : ''}`}>{i + 1}</Link>)}
-            <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
-                Next &rarr;
+                {Array.from({ length: totalPages }).map((_, i) => <Link key={`page${i}`} className={currentPage === 1 && i === 0 ? 'current' : ''} to={`${base}/${i > 0 ? i + 1 : ''}`}>{i + 1}</Link>)}
+            <Link title="Next page" disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+                <span className="word">Next</span> &rarr;
             </Link>
         </PaginationStyles>
     )
